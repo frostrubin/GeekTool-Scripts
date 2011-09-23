effcaa1b6cdcb44baf2cf6caa415beec6b85ae1a
frostrubin <blseeger@gmail.com>
2011-09-23 18:13:18 +0300

    Learned of unix command fold. Used it :)

--- network.sh
+++ network.sh
@@ -29,23 +29,17 @@ function external_ip {
 
 function vertical_ethernet_ip {
    ethernet_ip
-   for offset in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15;do 
-     echo ${address:$offset:1}
-   done
+   echo $address|fold -w 1
 }
 
 function vertical_airport_ip {
    airport_ip
-   for offset in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15;do 
-     echo ${address:$offset:1}
-   done
+   echo $address|fold -w 1
 } 
 
 function vertical_external_ip {
    external_ip
-   for offset in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15;do 
-     echo ${address:$offset:1}
-   done
+   echo $address|fold -w 1
 } 
 
 function all_in_one {
